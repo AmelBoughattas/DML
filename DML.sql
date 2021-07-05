d0a319270cafa6ebce37a51663f5a4e8130fe8d3
AmelBoughattas <amelbgh.misi@gmail.com>
2021-07-05 07:09:07 +0100

    checkpointdml

---
+++ DML.sql
@@ -0,0 +1,14 @@
+/*Data Manipulation language DML*/
+
+/*Insert values to table Product*/
+INSERT INTO Product VALUES ('P01', 'Samsung Galaxy S20', 'Smartphones', 3299)
+INSERT INTO Product VALUES ('P02', 'ASUS Notebook', 'PC', 4599)
+
+/*Insert values to table Customer*/
+INSERT INTO Customer VALUES ('C01', 'Ali', 71321009)
+INSERT INTO Customer VALUES ('C02', 'Asma', 77345823)
+
+/*Insert values to table Orders*/
+INSERT INTO Orders (Customer_id, Product_id, quantity, total_amount) VALUES ('C01', 'P02', 2, 9198)
+INSERT INTO Orders VALUES ('C02', 'P01', 28/05/2020/, 1, 3299)
+
